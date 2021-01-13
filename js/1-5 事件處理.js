0103895077e67010f4e020d0ad4e5c2301e45218
Fatleaf <cycfree1988@gmail.com>
2021-01-13 16:50:56 +0800

    update
    v-on,event,v-on修飾子,v-for

--- js/1-5 事件處理.js
+++ js/1-5 事件處理.js
@@ -11,6 +11,7 @@ const test01 = Vue.createApp({
     methods: {
         plus() {
             this.count2 = Number(this.count2 + 2); 
+            // console.log(event.target.tagName);
         },
         plus2() {
             this.count3 = Number(this.count3 + this.num);
@@ -22,4 +23,55 @@ const test01 = Vue.createApp({
 }).mount('#test01');
 //v-on:click 可簡寫為 @click
 
+//v-on,event
+const test02 = Vue.createApp({
+    data() {
+        return {
+            count: 0,
+            num: 0
+        }
+    },
+    methods: {
+        add(num, event) {
+            console.log(event.target.tagName);
+            this.count += num;
+        }
+    }
+}).mount('#test02');
+//跟我上面的第三個計數器一樣的結果...傳入指定事件>>$event
 
+//事件通用修飾子
+
+//stop
+const test03 = Vue.createApp({
+    methods: {
+        alert(val) {
+            alert(val);
+        }
+    }
+}).mount('#test03');
+//如果沒有掛上.stop則點擊內層的時候會向外擴張
+
+//4鍵盤按鍵
+const test04 = Vue.createApp({
+    data() {
+        return {
+            keypress: '',
+            messages: [],
+            msg: ''
+        }
+    },
+    methods: {
+        press(event){
+            this.keypress = event.key;
+            window.setTimeout(() => {
+                event.target.value = '';
+            }, 500);
+        },
+        addMessage(){
+            this.messages.push(this.msg);
+            this.msg = '';
+        }
+    }
+}).mount('#test04');
+//window.setTimeout( () => {...}, time))
